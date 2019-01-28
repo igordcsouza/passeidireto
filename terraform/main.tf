a8ec5eb0bacc88741935e38f0a6d4d37c9cf8009
Igor Souza <igordcsouza@me.com>
2019-01-27 22:07:30 -0200

    add terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,34 +1,38 @@
-provider "digitalocean" {
-}
+provider "digitalocean" {}
 
 resource "digitalocean_droplet" "web-app" {
- image  = "centos-7-x64"
- name   = "web-app"
- region = "${var.default_do_droplet_region}"
- size   = "${var.default_do_droplet_size}"
+  image    = "centos-7-x64"
+  name     = "web-app"
+  region   = "${var.default_do_droplet_region}"
+  size     = "${var.default_do_droplet_size}"
+  ssh_keys = "${var.default_ssh_keys}"
+  tags     = ["webapp", "baseline"]
 }
 
 resource "digitalocean_droplet" "database" {
- image  = "centos-7-x64"
- name   = "database"
- region = "${var.default_do_droplet_region}"
- size   = "${var.default_do_droplet_size}"
+  image    = "centos-7-x64"
+  name     = "database"
+  tags     = ["database", "baseline"]
+  region   = "${var.default_do_droplet_region}"
+  size     = "${var.default_do_droplet_size}"
+  ssh_keys = "${var.default_ssh_keys}"
 }
 
 resource "digitalocean_loadbalancer" "public" {
-  name = "loadbalancer-pd"
+  name   = "loadbalancer-pd"
   region = "${var.default_do_droplet_region}"
+  count  = 0
 
   forwarding_rule {
-    entry_port = 80
+    entry_port     = 80
     entry_protocol = "http"
-    
-    target_port = 80
+
+    target_port     = 80
     target_protocol = "http"
   }
 
   healthcheck {
-    port = 22
+    port     = 22
     protocol = "tcp"
   }
 
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -7,3 +7,10 @@ variable "default_do_droplet_region" {
   type    = "string"
   default = "nyc3"
 }
+
+variable "default_ssh_keys" {
+  default = [
+    "03:ab:e2:80:df:60:bb:fa:9a:b9:b5:9b:63:ef:0a:20",
+    "a3:01:cb:28:cc:95:e4:eb:7c:dc:f2:79:16:7c:7c:b2",
+  ]
+}
